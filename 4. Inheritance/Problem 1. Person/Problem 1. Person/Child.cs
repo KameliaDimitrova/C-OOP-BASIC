97bb81ab2885d813c0e63194a85766abe1b0808c
baskam <baskam@students.softuni.bg>
2018-02-24 14:59:16 +0200

    Add new task

---
+++ 4. Inheritance/Problem 1. Person/Problem 1. Person/Child.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class Child:Person
+{
+    private string name;
+
+    public string Name
+    {
+        get { return base.Name; }
+        protected set { base.Name = value; }
+    }
+
+    private int age;
+
+    public override int Age
+    {
+        get { return base.Age; }
+        protected set
+        {
+            if (value > 15)
+            {
+                throw new ArgumentException("Child's age must be less than 15!");
+            }
+            base.Age = value;
+        }
+    }
+
+    public Child(string name, int age):base(name,age)
+    {
+        
+    }
+
+}
+
---
+++ 4. Inheritance/Problem 1. Person/Problem 1. Person/Person.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class Person
+{
+    private string name;
+
+    public string Name
+    {
+        get { return name; }
+        protected set
+        {
+            if (value.Length < 3)
+            {
+                throw new ArgumentException("Name's length should not be less than 3 symbols!");
+            }
+            name = value;
+        }
+    }
+
+    private int age;
+
+    public virtual int Age
+    {
+        get { return age; }
+        protected set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Age must be positive!");
+            }
+            age = value;
+        }
+    }
+
+    public Person(string name, int age)
+    {
+        this.Name = name;
+        this.Age = age;
+    }
+
+    public override string ToString()
+    {
+        return $"Name: {this.name}, Age: {this.age}";
+    }
+}
+
