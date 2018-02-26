67ce4a7fbeb4e89f0bc52ced8d9ee9f77ae68d67
baskam <baskam@students.softuni.bg>
2018-02-26 17:49:13 +0200

    Add new SoftUni project Problem 6. Animals

---
+++ 4. Inheritance/Problem 6. Animals/Problem 6. Animals/Animal.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class Animal:ISoundProducable
+{
+
+    private const string ERORR_MESSAGE = "Invalid input!";
+    private string name;
+
+    public string Name
+    {
+        get { return name; }
+       private set
+        {
+            NotEmptyValidation(value);
+            name = value;
+        }
+    }
+
+    private int age;
+
+    public int Age
+    {
+        get { return age; }
+        private set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException(ERORR_MESSAGE);
+            }
+            age = value;
+        }
+    }
+
+    private string gender;
+
+    public string Gender
+    {
+        get { return gender; }
+        private set
+        {
+            NotEmptyValidation(value);
+            gender = value;
+        }
+    }
+
+    public Animal()
+    {
+    }
+
+    public Animal(string name, int age, string gender)
+    {
+        this.Name = name;
+        this.Age = age;
+        this.Gender = gender;
+    }
+
+    private static void NotEmptyValidation(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            throw new ArgumentException(ERORR_MESSAGE);
+        }
+    }
+
+    public virtual string ProduceSound()
+    {
+        return "NOISE!!!!!!!!";
+    }
+
+    public override string ToString()
+    {
+        return $"{this.GetType().Name}{Environment.NewLine}" +
+               $"{this.name} {this.age} {this.gender}{Environment.NewLine}" +
+               $"{this.ProduceSound()}{Environment.NewLine}";
+    }
+}
+
---
+++ 4. Inheritance/Problem 6. Animals/Problem 6. Animals/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Problem_6._Animals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> animals = new List<Animal>();
+
+            string animalType;
+            while ((animalType = Console.ReadLine()) != "Beast!")
+            {
+                try
+                {
+                    ReadAndCreateAnimals(animals, animalType);
+                }
+                catch (ArgumentException argEx)
+                {
+                    Console.WriteLine(argEx.Message);
+                }
+            }
+            PrintTheInformationForAllAnimals(animals);
+        }
+
+        private static void ReadAndCreateAnimals(List<Animal> animals, string animalType)
+        {
+            var tokens = Console.ReadLine()
+                .Split();
+            var animalName = tokens[0];
+            var age = int.Parse(tokens[1]);
+            switch (animalType)
+            {
+                case "Cat":
+                    Cat cat = new Cat(animalName, age, tokens[2]);
+                    animals.Add(cat);
+                    break;
+                case "Dog":
+                    Dog dog = new Dog(animalName, age, tokens[2]);
+                    animals.Add(dog);
+                    break;
+                case "Frog":
+                    Frog frog = new Frog(animalName, age, tokens[2]);
+                    animals.Add(frog);
+                    break;
+                case "Kitten":
+                    Kitten kiten = new Kitten(animalName, age);
+                    animals.Add(kiten);
+                    break;
+                case "Tomcat":
+                    Tomcat tomcat = new Tomcat(animalName, age);
+                    animals.Add(tomcat);
+                    break;
+                default:
+                   throw new ArgumentException("Invalid input!");
+            }
+        }
+
+        private static void PrintTheInformationForAllAnimals(List<Animal> animals)
+        {
+            foreach (var animal in animals)
+            {
+                Console.Write(animal);
+            }
+        }
+    }
+}
+
