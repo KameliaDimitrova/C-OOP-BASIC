b9d3b356b1f0c7492d1939ee29fc09d9dcdd4191
baskam <baskam@students.softuni.bg>
2018-02-26 15:37:18 +0200

    Add new project Problem 5. *Mordor’s Cruel Plan

---
+++ 4. Inheritance/Problem 5. Mordors Cruel Plan/Problem 5. Mordors Cruel Plan/Gandalf.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class Gandalf
+{
+    private readonly Dictionary<string, int> foodForHappiness = new Dictionary<string, int>()
+    {
+        {"cram", 2},
+        {"lembas", 3},
+        {"apple", 1},
+        {"melon", 1},
+        {"honeycake", 5},
+        {"mushrooms", -10},
+    };
+
+
+    private int pointsOfHappiness;
+
+    public int PointsOfHappiness
+    {
+        get { return pointsOfHappiness; }
+        set { pointsOfHappiness = value; }
+    }
+
+    public Gandalf()
+    {
+    }
+
+    public void EatFood(string food)
+    {
+        if (foodForHappiness.ContainsKey(food.ToLower()))
+        {
+            this.pointsOfHappiness = this.pointsOfHappiness + foodForHappiness[food.ToLower()];
+        }
+        else
+        {
+            this.pointsOfHappiness = this.pointsOfHappiness - 1;
+        }
+    }
+
+    public string GetGandalfMood()
+    {
+        if (this.pointsOfHappiness < -5)
+            return "Angry";
+        if (this.pointsOfHappiness <= 0)
+            return "Sad";
+        if (this.pointsOfHappiness <= 15)
+            return "Happy";
+        else
+            return "JavaScript";
+    }
+
+    public override string ToString()
+    {
+        return $"{this.pointsOfHappiness}{Environment.NewLine}" +
+               $"{GetGandalfMood()}{Environment.NewLine}";
+    }
+}
+
+
+
---
+++ 4. Inheritance/Problem 5. Mordors Cruel Plan/Problem 5. Mordors Cruel Plan/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Problem_5._Mordor_s_Cruel_Plan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gandalf gandalf=new Gandalf();
+            var input = Console.ReadLine()
+                .Split(' ')
+                .ToList();
+            for (int i = 0; i < input.Count; i++)
+            {
+                gandalf.EatFood(input[i]);
+            }
+
+            Console.Write(gandalf);
+        }
+    }
+}
