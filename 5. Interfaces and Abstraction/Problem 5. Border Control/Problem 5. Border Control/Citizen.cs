4209b19a60ba1070a6bf5b36335efcf6df85fca6
baskam <baskam@students.softuni.bg>
2018-02-28 16:23:55 +0200

    Add new Border control project

---
+++ 5. Interfaces and Abstraction/Problem 5. Border Control/Problem 5. Border Control/Citizen.cs
@@ -0,0 +1,35 @@
+﻿
+class Citizen:IEnter
+{
+    private string id;
+
+    public string Id
+    {
+        get { return id; }
+        set { id = value; }
+    }
+
+    private string name;
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    private int age;
+
+    public int Age
+    {
+        get { return age; }
+        set { age = value; }
+    }
+
+    public Citizen(string name, int age, string id)
+    {
+        this.Name = name;
+        this.Age = age;
+        this.Id = id;
+    }
+}
+
---
+++ 5. Interfaces and Abstraction/Problem 5. Border Control/Problem 5. Border Control/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem5.Border_Control
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           List<IEnter> enters=new List<IEnter>();
+           AddCitizenAndRobots(enters);
+           var idForCheck = Console.ReadLine();
+           Console.WriteLine(CheckForId(enters,idForCheck));
+        }
+
+        private static string CheckForId(List<IEnter> enters,string idForCheck)
+        {
+            var result = string.Empty;
+            foreach (var enter in enters)
+            {
+                if (enter.Id.EndsWith(idForCheck))
+                {
+                    result = result + enter.Id + Environment.NewLine;
+                }
+            }
+            return result;
+        }
+
+        public static void AddCitizenAndRobots(List<IEnter> enters)
+        {
+            var input = string.Empty;
+            while ((input = Console.ReadLine()) != "End")
+            {
+                var tokens = input
+                    .Split(' ')
+                    .ToList();
+                if (tokens.Count == 2)
+                {
+                    enters.Add(new Robot(tokens[0], tokens[1]));
+                }
+                else
+                {
+                    enters.Add(new Citizen(tokens[0], int.Parse(tokens[1]), tokens[2]));
+                }
+            }
+        }
+    }
+}
