75db62a7c11ec7d9fcd7434d472571ef180dc4ab
baskam <baskam@students.softuni.bg>
2018-02-28 15:04:48 +0200

    Add new interface project

---
+++ 5. Interfaces and Abstraction/Problem 2. Multiple Implementation/Problem 2. Multiple Implementation/Citizen.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Problem1.Define_Interface_IPerson;
+
+
+public class Citizen : IPerson,IBirthable,IIdentifiable
+{
+    private string name;
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    private int age;
+
+    public int Age
+    {
+        get { return age; }
+        set { age = value; }
+    }
+
+    private string id;
+
+    public string Id
+    {
+        get { return id; }
+        set { id = value; }
+    }
+
+    private string birthdate;
+
+    public string Birthdate
+    {
+        get { return birthdate; }
+        set { birthdate = value; }
+    }
+
+
+    public Citizen(string name, int age,string id,string birthDate)
+    {
+        this.Name = name;
+        this.Age = age;
+        this.Id = id;
+        this.Birthdate = birthDate;
+    }
+
+}
+
---
+++ 5. Interfaces and Abstraction/Problem 2. Multiple Implementation/Problem 2. Multiple Implementation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Problem1.Define_Interface_IPerson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = Console.ReadLine();
+            var age = int.Parse(Console.ReadLine());
+            var id = Console.ReadLine();
+            var birthdate = Console.ReadLine();
+            IIdentifiable identifiable = new Citizen(name, age, id, birthdate);
+            IBirthable birthable = new Citizen(name, age, id, birthdate);
+            Console.WriteLine(identifiable.Id);
+            Console.WriteLine(birthable.Birthdate);
+        }
+    }
+}
