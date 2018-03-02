91337b0a0245473de4a12642a03e16f1133f5bd5
baskam <baskam@students.softuni.bg>
2018-03-02 15:21:48 +0200

    Add ne project

---
+++ 5. Interfaces and Abstraction/Problem 10. Explicit Interfaces/Problem 10. Explicit Interfaces/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem_10._Explicit_Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IResident> allCitizens=new List<IResident>();
+            var input = String.Empty;
+            while ((input = Console.ReadLine()) != "End")
+            {
+                var tokens = input
+                    .Split(' ')
+                    .ToList();
+                var name = tokens[0];
+                var country = tokens[1];
+                var age = int.Parse(tokens[2]);
+                IResident citizen=new Citizen(name,country,age);
+                allCitizens.Add(citizen);
+            }
+            foreach (var citizen in allCitizens)
+            {
+                Console.WriteLine(citizen.Name);
+                Console.WriteLine(citizen.GetName());
+            }
+        }
+    }
+}
